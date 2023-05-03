067418669fcbace8b94265e1a32c2a74a5a045c7
acbgalan <acbgalan@gmail.com>
2023-05-04 01:01:21 +0200

    Repositories Interfaces

---
+++ src/Library/Server/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Server.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        T Get(int id);
+        List<T> GetAll();
+        void Update(T entity);
+        void Delete(int id);
+        void Delete(T entity);
+        bool Exits(int id);
+        int Save();
+    }
+}
