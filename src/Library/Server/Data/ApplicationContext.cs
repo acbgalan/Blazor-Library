b03ea8432e02e8389f453a1416746494ebd7be6d
acbgalan <acbgalan@gmail.com>
2023-05-04 00:20:34 +0200

    Contexto, cadena de conexión y registro como servicio en program.cs

---
+++ src/Library/Server/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Library.Server.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
