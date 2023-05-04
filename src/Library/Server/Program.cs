7963bd18db8815ae5fb48b9554fa98c2cc39bf7d
acbgalan <acbgalan@gmail.com>
2023-05-04 23:14:53 +0200

    Book repository and service config

--- src/Library/Server/Program.cs
+++ src/Library/Server/Program.cs
@@ -1,5 +1,7 @@
 global using Library.Shared;
+global using Library.Server.Repositories;
 using Library.Server.Data;
+using Library.Server.Repositories.Interfaces;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
