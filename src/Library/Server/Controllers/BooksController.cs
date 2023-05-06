c95b2022d7e89111cea223a3955b357059ccb7c3
acbgalan <acbgalan@gmail.com>
2023-05-06 02:38:36 +0200

    Controlador para libros, acción para devolver todos los libros

---
+++ src/Library/Server/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using Library.Server.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+        {
+            var books = await _bookRepository.GetAllAsync();
+            return Ok(books);
+        }
+
+
+    }
+}
