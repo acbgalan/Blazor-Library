605e6b103b1da9169de2762b3826f85fac5f1497
Carlos Blanco <cblanco@grupokh.com>
2023-05-08 18:45:23 +0200

    Servicio de categorias en cliente

---
+++ src/Library/Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+        public List<Category> Categories { get; set; }
+
+        public Task<List<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
